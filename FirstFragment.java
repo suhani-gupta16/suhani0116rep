e8a04d7a1f85666ce81f43273900794f0311e5cf
suhani-gupta16 <119233890+suhani-gupta16@users.noreply.github.com>
2023-04-16 18:20:36 -0400

    Add files via upload

---
+++ FirstFragment.java
@@ -0,0 +1,39 @@
+package com.example.stemifypage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.stemifypage.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
+
